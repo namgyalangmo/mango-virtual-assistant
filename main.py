bfc60e61916315c51debd5f60cabaa732fab6240
namgyalangmo <namgyalangmo2003@gmail.com>
2022-06-04 19:28:00 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+import pyttsx3
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+try:
+    with sr.Microphone() as source:
+        print('listening...')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        command = command.lower()
+        if 'mango' in command:
+            talk(command)
+except:
+    pass
